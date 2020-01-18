b3f81dfcdd8c62ea898e3e0a41a972a1746fb663
Yifeng Zhu <yifengz2@andrew.cmu.edu>
2020-01-18 17:34:40 -0600

    add cppzmq to conanfile requirement as zmq does not have zmq.hpp

--- src/formation_server.cc
+++ src/formation_server.cc
@@ -1,3 +1,5 @@
+#include "zmq.h"
+#include "zmq.hpp"
 #include "formation.pb.h"
 #include <vector>
 #include <string>
@@ -7,6 +9,12 @@ int main(){
   GOOGLE_PROTOBUF_VERIFY_VERSION;
   std::cout << "Hello World" << std::endl;
 
+  // Prepare the context and socket
+  zmq::context_t context(1);
+  zmq::socket_t socket(context, ZMQ_PAIR);
+
+  
+  
   int num = 5;
   std::vector<double> pos_x, pos_y;
   for (int i=0; i< 5; i++){
@@ -21,6 +29,10 @@ int main(){
   std::string msg_str;
   formation.SerializeToString(&msg_str);
 
-  std::cout << msg_str << std::endl;
-  
+  zmq::message_t request(msg_str.size());
+  memcpy((void*)request.data(), msg_str.c_str(), msg_str.size());
+
+  socket.send(request);
+  google::protobuf::ShutdownProtobufLibrary();
+  return 0;
 }
