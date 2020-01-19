7fe3a29d3b87cdebcfe9c924bd5bc15cac6877e8
Yifeng Zhu <yifengz2@andrew.cmu.edu>
2020-01-18 21:32:35 -0600

    An example of pub/sub img type

--- src/example_pub.cc
+++ src/example_pub.cc
@@ -1,22 +1,35 @@
 #include "zmq.hpp"
 #include "img.pb.h"
 #include <string>
+#include "CImg.h"
+using namespace cimg_library;
+
 int main(){
+
+  CImg<unsigned char> image("example.jpg");
+  // CImgDisplay main_disp(image);
+  // while (!main_disp.is_closed()){
+  //   main_disp.wait();
+  // }
+  
   zmq::context_t context(1);
   zmq::socket_t publisher(context, ZMQ_PUB);
 
   publisher.bind("tcp://*:5555");
 
   img::gray_img gray_img;
-  gray_img.set_width(128);
-  gray_img.set_height(128);
-  gray_img.set_img_data(std::string("this is a test string"));
+  gray_img.set_width(image.width());
+  gray_img.set_height(image.height());
+
+  
+  gray_img.set_img_data(reinterpret_cast<char*>(image.data()));
+  std::cout << image.data() << std::endl;
   std::string msg_str;
   gray_img.SerializeToString(&msg_str);
-
   while (true){
     // !!! Message must be built every time before you send them
     zmq::message_t message(msg_str.size());
+    std::cout << msg_str.size() << std::endl;
     memcpy(message.data(), msg_str.c_str(), msg_str.size());    
     publisher.send(message);
   }
--- src/example_sub.cc
+++ src/example_sub.cc
@@ -2,6 +2,11 @@
 #include "img.pb.h"
 #include <string>
 #include <vector>
+#include <google/protobuf/text_format.h>
+
+#include "CImg.h"
+using namespace cimg_library;
+
 // see the example here: https://ogbe.net/blog/zmq_helloworld.html
 int main(){
   zmq::context_t context(1);
@@ -23,10 +28,28 @@ int main(){
     // zmq::poll(p.data(), 1, -1);
     // if (p[0].revents & ZMQ_POLLIN){
     subscriber.recv(&recv_msg);
-    // std::string recv_str;
-    // recv_str.assign(static_cast<char *>(recv_msg.data()), recv_msg.size());
-    // gray_img.ParseFromString(recv_str);
-    std::cout << recv_msg.size() << std::endl;
+    std::string recv_str;
+    recv_str.assign(static_cast<char *>(recv_msg.data()), recv_msg.size());
+    gray_img.ParseFromString(recv_str);
+
+    std::cout << recv_str.size() << std::endl;
+    // print out the text string
+    // std::string text_str;
+    // google::protobuf::TextFormat::PrintToString(gray_img, &text_str);
+    // std::cout << text_str << std::endl;
+
+    auto img_data = reinterpret_cast<const uint8_t*>(gray_img.img_data().c_str());    
+    CImg<uint8_t> image(img_data, gray_img.width(), gray_img.height(), 1, 1);
+    CImgDisplay main_disp(image);
+    while (!main_disp.is_closed()){
+      main_disp.wait();
+    }
+    std::cout << "Width: " << gray_img.width() << " , " << "Height: " << gray_img.height() << std::endl;
+    std::cout << static_cast<int>(image(0, 0)) << std::endl;
+    std::cout << static_cast<int>(image(1, 1)) << std::endl;
+    std::cout << static_cast<int>(image(2, 3)) << std::endl;
+    std::cout << static_cast<int>(image(gray_img.width()-1, gray_img.height()-1)) << std::endl;
+    
     if (++nrx == nmsg){
       break;
       // }      
